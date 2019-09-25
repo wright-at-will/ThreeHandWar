36242e70867d77e24b658b8bc4f5edb146cb56b4
cjhammond20 <casey.j.hammond@gmail.com>
2019-09-25 17:59:56 -0500

    added iteration limit
    added default player names 'Bob' and 'Sue'
    
    implemented playGame(), playRound(), endGame(), printScore(), playCard(), winRound(), and tieRound()

--- src/main/java/war/Annihilation.java
+++ src/main/java/war/Annihilation.java
@@ -3,13 +3,30 @@ package war;
 import Deck.*;
 import players.Dealer;
 import players.Player;
-//This is the war that doesn't end until someone loses all cards
+//This is the war that ends after a set amount of rounds played. Whoever has the most cards at the end wins.
 public class Annihilation implements War{
     private Player player1,player2;
     private Dealer dealer;
     private Deck deck;
-    public boolean play(){
+    private int iterationLimit;
+    private boolean playing;
+
+    public boolean playGame(){
+        this.playing = true;
+        player1 = new Player("Bob");
+        player2 = new Player("Sue");
         dealer = new Dealer();
+        setHands();
+        iterationLimit = 100;
+
+        for (int i = 0; i <= iterationLimit; i++) {
+            if(playing)
+                playRound();
+            else
+                break;
+        }
+
+        endGame();
         return true;
     }
 
@@ -20,7 +37,56 @@ public class Annihilation implements War{
             dealer.giveCard(player2);
         }
     }
-    public void set(){
 
+    private void playRound() {
+        if (player1.getHand().getSize() == 0 || player2.getHand().getSize() == 0) {
+            this.playing = false;
+            return;
+        }
+        Card player1Card = playCard(player1);
+        Card player2Card = playCard(player2);
+        if (player1Card.compareTo(player2Card) > 1)
+            player1 = winRound(player1, player2Card, player1Card);
+        if (player1Card.compareTo(player2Card) < 1)
+            player2 = winRound(player2,player1Card,player2Card);
+        if (player1Card.compareTo(player2Card) == 0) {
+            tieRound();
+            return;
+        }
+        printScore();
+    }
+
+    private void endGame() {
+        System.out.println("Game Over");
+
+        if (player1.getHand().getSize() > player2.getHand().getSize())
+            System.out.println(player1.getName() + " Wins!");
+        else if (player1.getHand().getSize() < player2.getHand().getSize())
+            System.out.println(player2.getName() + " Wins!");
+        else
+            System.out.println("Tie Game!");
+    }
+
+    private Card playCard(Player player){
+        Card card = player.playCard();
+        System.out.println(player.getName() + " plays " + card.getValue() + " of " + card.getSuit() + " as up card");
+        return card;
+    }
+
+    private Player winRound(Player winner, Card newCard, Card originalCard) {
+        winner.addCard(originalCard);
+        winner.addCard(newCard);
+        System.out.println(winner.getName() + " wins the round");
+        return winner;
+    }
+
+    private void tieRound() {
+        System.out.println("War!");
+        playRound();
+    }
+
+    private void printScore() {
+        System.out.println("Score is " + player1.getName() + " " + player1.getHand().getSize()
+                + ", " + player2.getName() + " " + player2.getHand().getSize() + "\n");
     }
 }
