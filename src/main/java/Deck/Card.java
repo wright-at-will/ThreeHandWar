0565c0b76eef5f72d46f29da611eee67a24ba786
cjhammond20 <casey.j.hammond@gmail.com>
2019-09-25 18:02:40 -0500

    Capitalized enum values because that's what Dr. Robinson was doing in his examples. Feel free to change it back if you don't like it.

--- src/main/java/Deck/Card.java
+++ src/main/java/Deck/Card.java
@@ -31,40 +31,40 @@ public class Card {
     private void setValue(int value){
         switch (value){
             case 2:
-                this.value = Values.Two;
+                this.value = Values.TWO;
                 break;
             case 3:
-                this.value = Values.Three;
+                this.value = Values.THREE;
                 break;
             case 4:
-                this.value = Values.Four;
+                this.value = Values.FOUR;
                 break;
             case 5:
-                this.value = Values.Five;
+                this.value = Values.FIVE;
                 break;
             case 6:
-                this.value = Values.Six;
+                this.value = Values.SIX;
                 break;
             case 7:
-                this.value = Values.Seven;
+                this.value = Values.SEVEN;
                 break;
             case 8:
-                this.value = Values.Eight;
+                this.value = Values.EIGHT;
                 break;
             case 9:
-                this.value = Values.Nine;
+                this.value = Values.NINE;
                 break;
             case 10:
-                this.value = Values.Jack;
+                this.value = Values.JACK;
                 break;
             case 11:
-                this.value = Values.Queen;
+                this.value = Values.QUEEN;
                 break;
             case 12:
-                this.value = Values.King;
+                this.value = Values.KING;
                 break;
             case 13:
-                this.value = Values.Ace;
+                this.value = Values.ACE;
                 break;
         }
 
