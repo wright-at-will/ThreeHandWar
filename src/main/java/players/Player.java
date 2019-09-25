b7bd43f224ad7c9bd7cc133f84a70c823318f356
cjhammond20 <casey.j.hammond@gmail.com>
2019-09-25 18:05:31 -0500

    Added name variable
    Added getName()
    
    Added getHand()
    
    Added setHand()
     - May need to be removed because I'm no longer using it

--- src/main/java/players/Player.java
+++ src/main/java/players/Player.java
@@ -4,10 +4,12 @@ import Deck.Card;
 import Deck.Hand;
 
 public class Player {
-    Hand hand;
+    private String name;
+    private Hand hand;
 
-    public Player(){
-
+    public Player(String name){
+        this.name = name;
+        this.hand = new Hand();
     }
 
     public Card playCard(){
@@ -17,4 +19,10 @@ public class Player {
     public void addCard(Card card){
         hand.addToHand(card);
     }
+
+    public String getName() { return this.name; }
+
+    public Hand getHand() {  return this.hand; }
+
+    public void setHand(Hand hand) { this.hand = hand;}
 }
