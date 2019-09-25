50d97639554e8ead88f70c4165365086d07a3636
cjhammond20 <casey.j.hammond@gmail.com>
2019-09-25 18:07:56 -0500

    Added description
    changed set() and play() to setHands() and playGame() to match the interface

--- src/main/java/war/ThreeHandWar.java
+++ src/main/java/war/ThreeHandWar.java
@@ -1,15 +1,15 @@
 package war;
 
 import Deck.Hand;
-
+//This is the 3 player war, where all won cards are placed in a pile to the side
 public class ThreeHandWar implements War{
     private Hand player1,player2,player3;
 
-    public void set(){
+    public void setHands(){
 
     }
 
-    public boolean play(){
+    public boolean playGame(){
         return true;
     }
 }
